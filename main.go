58f22a749f3680e40482a3f1ff0d7b69b7e9bdd4
Messi Yang <dumdumgenius@gmail.com>
2022-10-07 00:51:31 +0800

    feat: make client and computer fully separated on development with docker-compose

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"os"
+
 	"github.com/dum-dum-genius/game-of-liberty-computer/gameclientcommunicator"
 	"github.com/dum-dum-genius/game-of-liberty-computer/gamecomputer"
 )
 
 func main() {
-	go gamecomputer.Start()
-	gameclientcommunicator.Start()
+	var app string
+	if app = os.Getenv("APP"); app == "" {
+		panic("You must set the 'APP'")
+	}
+
+	if app == "client" {
+		gameclientcommunicator.Start()
+	} else if app == "computer" {
+		gamecomputer.Start()
+	}
 }
