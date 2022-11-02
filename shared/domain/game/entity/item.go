a76f8110f08cb1548dae14e8814be0534d164cf5
Messi Yang <dumdumgenius@gmail.com>
2022-11-02 18:25:25 +0800

    feat: add Item entity

---
+++ shared/domain/game/entity/item.go
@@ -0,0 +1,20 @@
+package entity
+
+import "github.com/google/uuid"
+
+type Item struct {
+	id   uuid.UUID
+	name string
+}
+
+func NewItem(uuid uuid.UUID, name string) Item {
+	return Item{id: uuid, name: name}
+}
+
+func (item *Item) GetId() uuid.UUID {
+	return item.id
+}
+
+func (item *Item) GetName() string {
+	return item.name
+}
