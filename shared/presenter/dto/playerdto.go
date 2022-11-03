93af423f4b4bd276abd109a50fb6db7d7ed37687
Messi Yang <dumdumgenius@gmail.com>
2022-11-03 22:03:45 +0800

    refactor: change player to aggregate

--- shared/presenter/dto/playerdto.go
+++ shared/presenter/dto/playerdto.go
@@ -1,7 +1,7 @@
 package dto
 
 import (
-	"github.com/dum-dum-genius/game-of-liberty-computer/shared/domain/game/entity"
+	"github.com/dum-dum-genius/game-of-liberty-computer/shared/domain/game/aggregate"
 	"github.com/google/uuid"
 )
 
@@ -9,12 +9,12 @@ type PlayerDto struct {
 	Id uuid.UUID `json:"id"`
 }
 
-func NewPlayerDto(player entity.Player) PlayerDto {
+func NewPlayerDto(player aggregate.Player) PlayerDto {
 	return PlayerDto{
 		Id: player.GetId(),
 	}
 }
 
-func (dto PlayerDto) ToEntity() entity.Player {
-	return entity.NewPlayer(dto.Id)
+func (dto PlayerDto) ToEntity() aggregate.Player {
+	return aggregate.NewPlayer(dto.Id)
 }
