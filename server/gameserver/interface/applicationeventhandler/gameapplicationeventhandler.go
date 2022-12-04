aa768d8a840a64419b6416776f0276db6b73e782
Messi Yang <dumdumgenius@gmail.com>
2022-12-04 11:40:10 +0800

    refator: improve readibility in applicationeventhandler

--- server/gameserver/interface/applicationeventhandler/gameapplicationeventhandler.go
+++ server/gameserver/interface/applicationeventhandler/gameapplicationeventhandler.go
@@ -15,64 +15,72 @@ func NewGameIntegrationEventHandler(
 	configuration GameIntegrationEventHandlerConfiguration,
 ) {
 	redisReviveUnitsRequestedSubscriber, _ := redis.NewRedisReviveUnitsRequestedSubscriber()
-	redisReviveUnitsRequestedSubscriberUnsubscriber := redisReviveUnitsRequestedSubscriber.Subscribe(func(event commonredisdto.RedisReviveUnitsRequestedEvent) {
-		liveGameId, err := event.LiveGameId.ToValueObject()
-		if err != nil {
-			return
-		}
-		coordinates, err := commonjsondto.ParseCoordinateJsonDtos(event.Coordinates)
-		if err != nil {
-			return
-		}
-		configuration.LiveGameApplicationService.ReviveUnitsInLiveGame(liveGameId, coordinates)
-	})
+	redisReviveUnitsRequestedSubscriberUnsubscriber := redisReviveUnitsRequestedSubscriber.Subscribe(
+		func(event commonredisdto.RedisReviveUnitsRequestedEvent) {
+			liveGameId, err := event.LiveGameId.ToValueObject()
+			if err != nil {
+				return
+			}
+			coordinates, err := commonjsondto.ParseCoordinateJsonDtos(event.Coordinates)
+			if err != nil {
+				return
+			}
+			configuration.LiveGameApplicationService.ReviveUnitsInLiveGame(liveGameId, coordinates)
+		},
+	)
 	defer redisReviveUnitsRequestedSubscriberUnsubscriber()
 
 	redisAddPlayerRequestedSubscriber, _ := redis.NewRedisAddPlayerRequestedSubscriber()
-	redisAddPlayerRequestedSubscriberUnsubscriber := redisAddPlayerRequestedSubscriber.Subscribe(func(event commonredisdto.RedisAddPlayerRequestedEvent) {
-		liveGameId, err := event.LiveGameId.ToValueObject()
-		if err != nil {
-			return
-		}
-		playerId, err := event.PlayerId.ToValueObject()
-		if err != nil {
-			return
-		}
-		configuration.LiveGameApplicationService.AddPlayerToLiveGame(liveGameId, playerId)
-	})
+	redisAddPlayerRequestedSubscriberUnsubscriber := redisAddPlayerRequestedSubscriber.Subscribe(
+		func(event commonredisdto.RedisAddPlayerRequestedEvent) {
+			liveGameId, err := event.LiveGameId.ToValueObject()
+			if err != nil {
+				return
+			}
+			playerId, err := event.PlayerId.ToValueObject()
+			if err != nil {
+				return
+			}
+			configuration.LiveGameApplicationService.AddPlayerToLiveGame(liveGameId, playerId)
+		},
+	)
 	defer redisAddPlayerRequestedSubscriberUnsubscriber()
 
 	redisRemovePlayerRequestedSubscriber, _ := redis.NewRedisRemovePlayerRequestedSubscriber()
-	redisRemovePlayerRequestedSubscriberUnsubscriber := redisRemovePlayerRequestedSubscriber.Subscribe(func(event commonredisdto.RedisRemovePlayerRequestedEvent) {
-		liveGameId, err := event.LiveGameId.ToValueObject()
-		if err != nil {
-			return
-		}
-		playerId, err := event.PlayerId.ToValueObject()
-		if err != nil {
-			return
-		}
-		configuration.LiveGameApplicationService.RemovePlayerFromLiveGame(liveGameId, playerId)
-		configuration.LiveGameApplicationService.RemoveZoomedAreaFromLiveGame(liveGameId, playerId)
-	})
+	redisRemovePlayerRequestedSubscriberUnsubscriber := redisRemovePlayerRequestedSubscriber.Subscribe(
+		func(event commonredisdto.RedisRemovePlayerRequestedEvent) {
+			liveGameId, err := event.LiveGameId.ToValueObject()
+			if err != nil {
+				return
+			}
+			playerId, err := event.PlayerId.ToValueObject()
+			if err != nil {
+				return
+			}
+			configuration.LiveGameApplicationService.RemovePlayerFromLiveGame(liveGameId, playerId)
+			configuration.LiveGameApplicationService.RemoveZoomedAreaFromLiveGame(liveGameId, playerId)
+		},
+	)
 	defer redisRemovePlayerRequestedSubscriberUnsubscriber()
 
 	redisZoomAreaRequestedSubscriber, _ := redis.NewRedisZoomAreaRequestedSubscriber()
-	redisZoomAreaRequestedSubscriberUnsubscriber := redisZoomAreaRequestedSubscriber.Subscribe(func(event commonredisdto.RedisZoomAreaRequestedEvent) {
-		liveGameId, err := event.LiveGameId.ToValueObject()
-		if err != nil {
-			return
-		}
-		playerId, err := event.PlayerId.ToValueObject()
-		if err != nil {
-			return
-		}
-		area, err := event.Area.ToValueObject()
-		if err != nil {
-			return
-		}
-		configuration.LiveGameApplicationService.AddZoomedAreaToLiveGame(liveGameId, playerId, area)
-	})
+	redisZoomAreaRequestedSubscriberUnsubscriber := redisZoomAreaRequestedSubscriber.Subscribe(
+		func(event commonredisdto.RedisZoomAreaRequestedEvent) {
+			liveGameId, err := event.LiveGameId.ToValueObject()
+			if err != nil {
+				return
+			}
+			playerId, err := event.PlayerId.ToValueObject()
+			if err != nil {
+				return
+			}
+			area, err := event.Area.ToValueObject()
+			if err != nil {
+				return
+			}
+			configuration.LiveGameApplicationService.AddZoomedAreaToLiveGame(liveGameId, playerId, area)
+		},
+	)
 	defer redisZoomAreaRequestedSubscriberUnsubscriber()
 
 	closeConnFlag := make(chan bool)
