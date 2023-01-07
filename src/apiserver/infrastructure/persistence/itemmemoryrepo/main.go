4d0a92760e3962f31cb8c0e8c1dfb5ae3747424c
Messi Yang <dumdumgenius@gmail.com>
2023-01-07 16:00:43 +0800

    feat: add stone and torch items

--- src/apiserver/infrastructure/persistence/itemmemoryrepo/main.go
+++ src/apiserver/infrastructure/persistence/itemmemoryrepo/main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/dum-dum-genius/game-of-liberty-computer/src/domain/model/itemmodel"
+	"github.com/google/uuid"
 )
 
 type memoryRepo struct {
@@ -19,6 +20,12 @@ func New() itemmodel.Repo {
 		bananaItemDefaultId, _ := itemmodel.NewItemId("7ab5bc4e-9596-4a54-adee-8807512cbbb4")
 		orangeItemDefaultId, _ := itemmodel.NewItemId("3e8a5704-6de6-4156-96ea-25076aa82b35")
 		waterMelonItemDefaultId, _ := itemmodel.NewItemId("6c2da2d6-d47b-4bc3-b884-889f9b7ba882")
+		stoneItemDefaultId, _ := itemmodel.NewItemId("2a0c8f7f-48dc-4553-86b4-2bbc3786bb66")
+		torchItemDefaultId, _ := itemmodel.NewItemId("31c8cc9e-42a3-4a42-86d2-905ca37305ba")
+
+		fmt.Println(uuid.New())
+		fmt.Println(uuid.New())
+		fmt.Println(uuid.New())
 
 		serverUrl := os.Getenv("SERVER_URL")
 
@@ -28,6 +35,8 @@ func New() itemmodel.Repo {
 				itemmodel.New(bananaItemDefaultId, "banana", fmt.Sprintf("%s/assets/items/banana.png", serverUrl)),
 				itemmodel.New(orangeItemDefaultId, "orange", fmt.Sprintf("%s/assets/items/orange.png", serverUrl)),
 				itemmodel.New(waterMelonItemDefaultId, "water melon", fmt.Sprintf("%s/assets/items/water-melon.png", serverUrl)),
+				itemmodel.New(stoneItemDefaultId, "stone", fmt.Sprintf("%s/assets/items/stone.png", serverUrl)),
+				itemmodel.New(torchItemDefaultId, "torch", fmt.Sprintf("%s/assets/items/torch.png", serverUrl)),
 			},
 		}
 		return singleton
