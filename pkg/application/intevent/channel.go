ac4c75c899fb932b2223e9f6892f2d92029f6d3d
Messi Yang <dumdumgenius@gmail.com>
2023-02-11 20:18:48 +0800

    refactor: remove unused CreateGameAdminChannel

--- pkg/application/intevent/channel.go
+++ pkg/application/intevent/channel.go
@@ -4,10 +4,6 @@ import (
 	"fmt"
 )
 
-func CreateGameAdminChannel() string {
-	return "GAME_ADMIN"
-}
-
 func CreateGameClientChannel(gameId string, playerId string) string {
 	return fmt.Sprintf("GAME_%s_CLIENT_PLAYER_%s", gameId, playerId)
 }
