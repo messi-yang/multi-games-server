638505d8b7b6bbcb43ef6cc88e8655112db3be1d
Messi Yang <dumdumgenius@gmail.com>
2023-05-17 01:00:42 +0800

    chore: remove unused provideGamerAppService

--- pkg/interface/http/httphandler/worldhttphandler/provider.go
+++ pkg/interface/http/httphandler/worldhttphandler/provider.go
@@ -1,18 +1,11 @@
 package worldhttphandler
 
 import (
-	"github.com/dum-dum-genius/game-of-liberty-computer/pkg/context/game/application/service/gamerappsrv"
 	"github.com/dum-dum-genius/game-of-liberty-computer/pkg/context/game/application/service/worldappsrv"
 	"github.com/dum-dum-genius/game-of-liberty-computer/pkg/context/game/infrastructure/persistence/postgres/pgrepo"
 	"github.com/dum-dum-genius/game-of-liberty-computer/pkg/context/sharedkernel/infrastructure/event/memory/memdomainevent"
 	"github.com/dum-dum-genius/game-of-liberty-computer/pkg/context/sharedkernel/infrastructure/persistence/postgres/pguow"
 )
-
-func provideGamerAppService(uow pguow.Uow) gamerappsrv.Service {
-	gamerRepo := pgrepo.NewGamerRepo(uow)
-	domainEventDispatcher := memdomainevent.NewDispatcher(uow)
-	return gamerappsrv.NewService(gamerRepo, domainEventDispatcher)
-}
 
 func provideWorldAppService(uow pguow.Uow) worldappsrv.Service {
 	worldRepo := pgrepo.NewWorldRepo(uow)
