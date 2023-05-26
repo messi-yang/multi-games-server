d8e47ff1bc2960a4fb393604cb1e4b38d4a54041
Messi Yang <dumdumgenius@gmail.com>
2023-05-26 23:49:50 +0800

    fix: fix the bug that gamer cannot create worlds when limit is not reached

--- pkg/context/game/application/dto/gamer_dto.go
+++ pkg/context/game/application/dto/gamer_dto.go
@@ -6,17 +6,15 @@ import (
 )
 
 type GamerDto struct {
-	Id               uuid.UUID `json:"id"`
-	UserId           uuid.UUID `json:"userId"`
-	WorldsCount      int8      `json:"worldsCount"`
-	WorldsCountLimit int8      `json:"worldsCountLimit"`
+	Id          uuid.UUID `json:"id"`
+	UserId      uuid.UUID `json:"userId"`
+	WorldsCount int8      `json:"worldsCount"`
 }
 
 func NewGamerDto(gamer gamermodel.Gamer) GamerDto {
 	return GamerDto{
-		Id:               gamer.GetId().Uuid(),
-		UserId:           gamer.GetUserId().Uuid(),
-		WorldsCount:      gamer.GetWorldsCount(),
-		WorldsCountLimit: gamer.GetWorldsCountLimit(),
+		Id:          gamer.GetId().Uuid(),
+		UserId:      gamer.GetUserId().Uuid(),
+		WorldsCount: gamer.GetWorldsCount(),
 	}
 }
