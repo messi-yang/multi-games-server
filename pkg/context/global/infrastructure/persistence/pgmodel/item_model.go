f61c540c87b0287bcfb25ec51980dcf1a3f0e747
Messi Yang <dumdumgenius@gmail.com>
2023-12-30 18:18:56 +0800

    fix: add specific type to ItemModel

--- pkg/context/global/infrastructure/persistence/pgmodel/item_model.go
+++ pkg/context/global/infrastructure/persistence/pgmodel/item_model.go
@@ -12,7 +12,7 @@ type ItemModel struct {
 	CompatibleUnitType UnitTypeEnum   `gorm:"not null"`
 	Name               string         `gorm:"not null"`
 	Traversable        bool           `gorm:"not null"`
-	ModelSources       pq.StringArray `gorm:"not null"`
+	ModelSources       pq.StringArray `gorm:"not null;type:text[]"`
 	ThumbnailSrc       string         `gorm:"not null"`
 	CreatedAt          time.Time      `gorm:"autoCreateTime;not null"`
 	UpdatedAt          time.Time      `gorm:"autoUpdateTime;not null"`
