9e07b28621c088b70dfb895d93cc64389bdbcdb6
Messi Yang <dumdumgenius@gmail.com>
2024-03-21 20:33:46 +0800

    refactor: sort out commonutil package

--- pkg/util/commonutil/common_util.go
+++ pkg/util/commonutil/common_util.go
@@ -13,26 +13,10 @@ func MapWithError[T any, M any](inputArray []T, transformer func(i int, obj T) (
 	return outputArray, nil
 }
 
-func MapMatrix[T any, M any](inputMatrix [][]T, transformer func(i int, j int, obj T) (M, error)) ([][]M, error) {
-	outputMatrix := make([][]M, 0)
-
-	for i := 0; i < len(inputMatrix); i += 1 {
-		outputMatrix = append(outputMatrix, make([]M, 0))
-		for j := 0; j < len(inputMatrix[0]); j += 1 {
-			transformedObj, err := transformer(i, j, inputMatrix[i][j])
-			if err != nil {
-				return nil, err
-			}
-			outputMatrix[i] = append(outputMatrix[i], transformedObj)
-		}
-	}
-	return outputMatrix, nil
-}
-
-func RangeMatrix(width int, height int, callback func(i int, j int) error) error {
-	for i := 0; i < width; i += 1 {
-		for j := 0; j < height; j += 1 {
-			err := callback(i, j)
+func RangeMatrix(width int, height int, callback func(x int, z int) error) error {
+	for x := 0; x < width; x += 1 {
+		for z := 0; z < height; z += 1 {
+			err := callback(x, z)
 			if err != nil {
 				return err
 			}
